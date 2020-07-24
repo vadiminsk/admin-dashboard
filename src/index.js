6ed04acc5115e2ae279d5161718aec525334688b
Vadim <vadim@ib.by>
2020-07-24 17:05:56 +0300

    add menu component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/index';
+import PrimarySearchAppBar from './components/App/index';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <PrimarySearchAppBar/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
